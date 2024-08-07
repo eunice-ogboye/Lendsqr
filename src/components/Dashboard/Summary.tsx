3263d5516ce200cfb29524e54b5e2c32a512d685
eunice-ogboye <ogboyeeunice@gmail.com>
2024-08-07 20:04:53 +0100

    cards updated

--- src/components/Dashboard/Summary.tsx
+++ src/components/Dashboard/Summary.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import '../../assets/styles/Summary.css'
+import Cards from './Card'
+import DataTableComponent from './dataTable'
 
 function Summary() {
   return (
     <div className='details'>
         <h3>User</h3>
+        <Cards />
+        <DataTableComponent />
     </div>
   )
 }
