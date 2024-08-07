ce852df0b3034d72ab3d56496ec6e80fc89d03bf
eunice-ogboye <ogboyeeunice@gmail.com>
2024-08-07 20:01:36 +0100

    feat: side ba component added

--- src/components/common/BorrowersNav.tsx
+++ src/components/common/BorrowersNav.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaUsers, FaShieldAlt, FaMoneyBill, FaPiggyBank, FaCheck, FaChartBar } from 'react-icons/fa';
+import { FaUsers, FaShieldAlt, FaMoneyBill, FaPiggyBank } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import './BorrowersNav.css';
 
@@ -8,25 +8,38 @@ const BorrowersNav: React.FC = () => {
     { name: 'Users', icon: <FaUsers />, path: '/users' },
     { name: 'Guarantors', icon: <FaShieldAlt />, path: '/guarantors' },
     { name: 'Loan', icon: <FaMoneyBill />, path: '/loan' },
+    { name: 'Decision Models', icon: <FaMoneyBill />, path: '/loan' },
     { name: 'Savings', icon: <FaPiggyBank />, path: '/savings' }, 
-    { name: 'Users', icon: <FaUsers />, path: '/users' },
+    { name: 'Loan Requests', icon: <FaUsers />, path: '/users' },
+    { name: 'Whitelist', icon: <FaShieldAlt />, path: '/whitelist' },
+    { name: 'Karma', icon: <FaMoneyBill />, path: '/karma' },
+  ];
+  const businessMenus = [
+    { name: 'Organization', icon: <FaUsers />, path: '/users' },
+    { name: 'Loan Products', icon: <FaMoneyBill />, path: '/loan' },
+    { name: 'Saving Products', icon: <FaShieldAlt />, path: '/guarantors' },
     { name: 'Guarantors', icon: <FaShieldAlt />, path: '/guarantors' },
-    { name: 'Loan', icon: <FaMoneyBill />, path: '/loan' },
-    { name: 'Savings', icon: <FaPiggyBank />, path: '/savings' },
-    { name: 'Users', icon: <FaUsers />, path: '/users' },
-    { name: 'Guarantors', icon: <FaShieldAlt />, path: '/guarantors' },
-    { name: 'Loan', icon: <FaMoneyBill />, path: '/loan' },
-    { name: 'Savings', icon: <FaPiggyBank />, path: '/savings' },
-   
-    // Add more menus here...
+    { name: 'Decision Models', icon: <FaMoneyBill />, path: '/loan' },
+    { name: 'Savings', icon: <FaPiggyBank />, path: '/savings' }, 
+    { name: 'Loan Requests', icon: <FaUsers />, path: '/users' },
+    { name: 'Whitelist', icon: <FaShieldAlt />, path: '/whitelist' },
+    { name: 'Karma', icon: <FaMoneyBill />, path: '/karma' },
   ];
 
   return (
     <div className="side-nav">
+      <h4 className="menu-item">CUSTOMERS</h4>
       {menus.map((menu, index) => (
         <Link to={menu.path} key={index} className="menu-item">
           <span className="icon">{menu.icon}</span>
           <span className="name">{menu.name}</span>
+        </Link>
+      ))}
+      <h4 className="menu-item">BUSINESS</h4>
+      {businessMenus.map((bmenu, index) => (
+        <Link to={bmenu.path} key={index} className="menu-item">
+          <span className="icon">{bmenu.icon}</span>
+          <span className="name">{bmenu.name}</span>
         </Link>
       ))}
     </div>
